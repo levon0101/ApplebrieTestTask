d0f4efd54ef3b2052e207faf35f3276a934db011
Levon <levon.ecs@gmail.com>
2020-04-12 19:34:09 +0400

    *Request model izolaiting via Automapper *Finished Task

---
+++ ApplebrieTestTask.WebApi/Dto/CategoryDty.cs
@@ -0,0 +1,12 @@
+﻿using ApplebrieTestTask.WebApi.Entitities;
+
+namespace ApplebrieTestTask.WebApi.Dto
+{
+    public class CategoryDty
+    {
+        public int Id { get; set; } 
+
+        public string Name { get; set; }
+         
+    }
+}
---
+++ ApplebrieTestTask.WebApi/Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ApplebrieTestTask.WebApi.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
