174897bc78bb3b65d69ad7faf1c20a26d3201660
Levon <levon.ecs@gmail.com>
2020-04-19 22:30:18 +0400

    Removed Ignored File

---
+++ ApplebrieTestTask.WebApi/Dto/CategoryDto.cs
@@ -0,0 +1,12 @@
+﻿using ApplebrieTestTask.WebApi.Entitities;
+
+namespace ApplebrieTestTask.WebApi.Dto
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; } 
+
+        public string Name { get; set; }
+         
+    }
+}
